fb65428450352bdfc5c29cc13a7ac12b9a1917c5
Phan Quang Lam <lam.phan.qg@gmail.com>
2019-08-20 22:04:25 +0900

    Simplify code

--- lib/chapter.rb
+++ lib/chapter.rb
@@ -9,37 +9,32 @@ class Chapter
     @page = get_parsed_response
   end
 
-  def info
-    number = get_number
-    title = get_title
-    content = get_content
-    return number, title, content
+  def content
+    content_node = @page.at_css(".chapter-c")
+    content_node.inner_html
   end
 
   def next_chap_link
-    get_next_chap_link
+    next_chap_node = @page.at_css("#next_chap")
+    return "" if next_chap_node["class"].include?("disabled")
+    next_chap_node["href"]
+  end
+
+  def number
+    @link.scan(/\d/).join
+  end
+
+  def title
+    title_node = @page.at_css(".chapter-title")
+    title_node["title"]
   end
 
   def truyen_title
-    get_truyen_title
+    truyen_title_node = @page.at_css("a.truyen-title")
+    truyen_title_node["title"]
   end
 
   private
-    def get_content
-      content_node = @page.at_css(".chapter-c")
-      content_node.inner_html
-    end
-
-    def get_next_chap_link
-      next_chap_node = @page.at_css("#next_chap")
-      return "" if next_chap_node["class"].include?("disabled")
-      next_chap_node["href"]
-    end
-
-    def get_number
-      @link.scan(/\d/).join
-    end
-
     def get_parsed_response
       res = get_response
       Nokogiri::HTML(res.body)
@@ -47,15 +42,5 @@ class Chapter
 
     def get_response
       Faraday.get(@link)
-    end
-
-    def get_title
-      title_node = @page.at_css(".chapter-title")
-      title_node["title"]
-    end
-
-    def get_truyen_title
-      truyen_title_node = @page.at_css("a.truyen-title")
-      truyen_title_node["title"]
     end
 end
